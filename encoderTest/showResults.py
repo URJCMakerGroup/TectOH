39ed3a33c8d5ee76581ce5eac4fb60794f4d83e4
rubennc91 <ruben3101@gmail.com>
2022-06-01 22:31:32 +0200

    Updated the python file to show the results. np.unwrap works, but... fine?

--- encoderTest/showResults.py
+++ encoderTest/showResults.py
@@ -1,10 +1,10 @@
-import matplotlib
-matplotlib.use('TkAgg') 
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
+from scipy import signal
  
 filename = sys.argv[1]
-print(filename)
+print("File to show " + filename)
 
 with open(filename,"rb") as f:
     raw = f.read()
@@ -15,10 +15,26 @@ data = list(range(len(raw)))
 for x in range(len(raw)):
     data[x] = raw[x]
     
-print(len(time))
-print(len(data))
+print("Samples read " + len(data))
 
-plt.plot(time, data)
+arr_data = np.array(data, dtype=int)
+unwrap_data = np.unwrap(arr_data, discont=125) # Works fine?
+
+plt.plot(time, unwrap_data)
 plt.xlabel('Samples')
 plt.ylabel('Position (??)')
 plt.show()
+
+
+# def compare_signal(raw_signal, **kwargs):
+    # fig, ax1 = plt.subplots(1, 1)
+    # ax1.plot(raw_signal, 'k-', label='Raw Signal')
+    # for i, (sig_name, sig_value) in enumerate(kwargs.items()):
+        # ax1.plot(sig_value, '-.', lw=4, alpha=0.5, label=sig_name)
+    # ax1.legend()
+    # plt.show()  
+    
+# simple_1d = np.arange(0, 1024, 4)
+# simple_1d_wrapped = simple_1d % 255
+# compare_signal(simple_1d, Wrapped=simple_1d_wrapped)
+# compare_signal(simple_1d, Wrapped=simple_1d_wrapped, Unwrapped=np.unwrap(simple_1d_wrapped, discont=127))

3dfd758cbf5382de97ddd3676253524cfcc6a88d
Felipe Machado <felipe.msz@gmail.com>
2022-09-25 20:53:44 +0100

    Process sensor data to consider overflow

--- encoderTest/magn_encoder_ana.py
+++ encoderTest/magn_encoder_ana.py
@@ -21,8 +21,8 @@ DIR = "./files/"
 
 data_filename = "capture_10_1.bin"
 data_filename = "capture_25_1.bin"
-data_filename = "capture_50_1.bin"
-data_filename = "capture_82_1.bin"
+#data_filename = "capture_50_1.bin"
+#data_filename = "capture_82_1.bin"
 #data_filename = "capture_100_1.bin"
 
 data_fulfilename = DIR + data_filename
@@ -50,7 +50,7 @@ for data_i in range(numdata):
     data.append(raw[data_i])
     
 # do the median filter
-window = 41
+window = 81
 # take window an odd value, same number of data in each side
 if window % 2 == 0:
     window + 1
@@ -69,6 +69,11 @@ time = [] # although we are filtering, it is ok to start at zero
 primer = 1
 base = 0 # will increase or decrease if the data goes above 255 or below 0
 
+
+BACK = 8000
+
+DBG_INDX = 129788*4
+
 for index, data_i in enumerate(data):
     # example window=9, side_window=4
     #         numdata=20 (0 to 19)
@@ -77,26 +82,82 @@ for index, data_i in enumerate(data):
     
     if index >= side_window and index < numdata-side_window:
         data_window = data[index-side_window:index+side_window+1]
-        # check if data_window has gone out of boundaries
-        
+
+        # go back 8000 positions to see the value
+        if index > BACK + side_window:
+            # now we get the tendency by going back BACK points
+            back_index = index - BACK
+            base_floor = 256 * int(median_data[back_index]/256) # take the base
+        else:
+            back_index = side_window
+            base_floor = 0
+        base_ceil  = base_floor + 256 
+        # check if some elements of data_window has gone out of boundaries
+        adjust_data_window = []
+        if index == DBG_INDX:
+            print(data_window)
+        if max(data_window) - min(data_window) > 200:
+            if index == DBG_INDX:
+                print(max(data_window))
+                print (np.median(data[back_index-side_window:back_index+side_window+1]))
+            # it means that it has overflow
+            if np.median(data[back_index-side_window:back_index+side_window+1]) > 127:
+                # we are going up
+                for datawin_i in data_window:
+                    if datawin_i < 127:
+                        adjust_data_window.append(base_ceil + datawin_i)
+                    else:
+                        adjust_data_window.append(base_floor + datawin_i)
+                if primer == 1:
+                    print(datawin)
+                    print(base_ceil)
+                    print(base_floor)
+                    primer = 0
+                if index == DBG_INDX:
+                    print(adjust_data_window)
+                    print(base_ceil)
+                    print(base_floor)
+
+                base = base_ceil # the next base will be ceil
+                
+            else: # going down
+                for datawin_i in data_window:
+                    if datawin_i > 127:
+                        adjust_data_window.append(base_floor - 256 + datawin_i)
+                    else:
+                        adjust_data_window.append(base_floor + datawin_i)
+                base = base_floor-256 # the next base will be floor
+                if index == DBG_INDX:
+                    print(adjust_data_window)
+                    print(base_ceil)
+                    print(base_floor)
+        else: # no overflow
+            for datawin_i in data_window:
+                adjust_data_window.append(base + datawin_i) # here base is used
+              
         orig_data.append(data[index])
-        median_data.append(int(np.median(data_window)))
-        mean_data.append(round(np.mean(data_window),1))
-        mean_data_int.append(int(round(np.mean(data_window),0)))
+        median_data.append(int(np.median(adjust_data_window)))
+        if index == DBG_INDX:
+            print(median_data[-1])
+        mean_data.append(round(np.mean(adjust_data_window),1))
+        mean_data_int.append(int(round(np.mean(adjust_data_window),0)))
 
         time.append(0.25 * (index-side_window))
+        # double 
         if primer==1:
             #print(data_window)
             primer = 0
 
 side2_window = int(side_window/2)
 median2_data = []
+trend = [] 
 for index, data_i in enumerate(median_data):
     if index >= side2_window and index < numdata-side2_window:
         data_window = median_data[index-side2_window:index+side2_window+1]
         median2_data.append(int(np.median(data_window)))
     else:
         median2_data.append(data_i)
+      
     
 
             
@@ -105,7 +166,7 @@ plt.plot(time, median2_data, linewidth = 3, color='k')
 plt.plot(time, orig_data, linewidth=0.5, color='b', linestyle='dotted')
 plt.plot(time, median_data, color='r')
 plt.plot(time, mean_data, color='g')
-plt.plot(time, mean_data_int, color='m', linewidth = 1, linestyle='dotted')
+#plt.plot(time, mean_data_int, color='m', linewidth = 1, linestyle='dotted')
 plt.xlabel('Time')
 plt.ylabel('Position (??)')
 plt.show()

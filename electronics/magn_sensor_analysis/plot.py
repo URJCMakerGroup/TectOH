3b4e6b78c59573b34f44b79c3e4ad94408a6192f
Felipe Machado <felipe.msz@gmail.com>
2022-11-21 00:36:47 +0000

    MagnSens: plot 2nd pass moving average
    modified:   magn_sensor_analysis/plot.py

--- electronics/magn_sensor_analysis/plot.py
+++ electronics/magn_sensor_analysis/plot.py
@@ -12,24 +12,25 @@ import numpy as np
 
 # directory where the csv files are
 #DIR = "./files/"
-DIR = "./files_nov/"
+#DIR = "./files_nov/"
+DIR = "./files_nov2/"
 
 # ow.walk [2] are filenames
 dir_list = next(os.walk(DIR))[2]
 
-# take only the reduced csv files, the en with _red.csv
-redcsv_list = [redcsv for redcsv in dir_list if redcsv.endswith('_red.csv')]
+# could be reduced or not
+csv_list = [csv for csv in dir_list if csv.endswith('.csv')]
 
-if not redcsv_list: # list is false if empty
-    print("No reduced csv file found in resuls directory: " + DIR)
+if not csv_list: # list is false if empty
+    print("No csv file found in resuls directory: " + DIR)
     exit()
 
-redcsv_list.sort()
+csv_list.sort()
 
 # without the extension:
-csv_base_list = [pathlib.Path(redcsvfile).stem for redcsvfile in redcsv_list]
+csv_base_list = [pathlib.Path(csvfile).stem for csvfile in csv_list]
 
-print ("  --- reduced csv list:\n" + '\n'.join(csv_base_list))
+print ("  --- csv list:\n" + '\n'.join(csv_base_list))
 
 print('\n  Entry data name to generate plot:\n\n')
 csv_name = input()
@@ -52,8 +53,12 @@ red_time      = []
 red_median2   = []
 red_median1   = []
 red_mean      = []
+red_mean2     = []
 red_mean_int  = []
+red_orig_basedata = []
 red_orig_data = []
+
+mean2_included = False
 
 with open(csv_fulfilename,"r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -61,27 +66,42 @@ with open(csv_fulfilename,"r") as csv_file:
     for csv_row in csv_reader:
         if first_row: # first row has the header
             first_row = False
+            if len(csv_row) == 9:
+                mean2_included = True # it was not in the beginning
         else:
             red_time.append(float(csv_row[1])) # 0 is the index
             red_median2.append(float(csv_row[2]))
             red_median1.append(float(csv_row[3]))
             red_mean.append(float(csv_row[4]))
             red_mean_int.append(float(csv_row[5]))
-            red_orig_data.append(int(csv_row[6]))
+            red_orig_basedata.append(int(csv_row[6]))
+            red_orig_data.append(int(csv_row[7]))
+            if mean2_included:
+                red_mean2.append(float(csv_row[8]))
                             
 # ------------- draw plots
 
-max_value = max(red_median2)
+#level = 15480 # change value if you want to see the base at certain level
+level = 0
+if level != 0:
+    red_orig_data = np.asarray(red_orig_data)
+    red_orig_data += level
+
+#max_value = max(red_median2)
+max_value = max(red_orig_basedata)
 
 fig, ax = plt.subplots()
 ax.plot(red_time, red_median2, linewidth = 3, color='k', label='median 2')
+ax.plot(red_time, red_orig_basedata, linewidth=0.5, color='c', linestyle='dotted', label='original basedata')
 ax.plot(red_time, red_orig_data, linewidth=0.5, color='b', linestyle='dotted', label='original data')
-ax.plot(red_time, red_median1, color='r', label= 'median 1')
+#ax.plot(red_time, red_median1, color='r', label= 'median 1')
 ax.plot(red_time, red_mean, color='g', label = 'mean')
 #ax.plot(red_time, red_mean_int, color='m', linewidth = 1, linestyle='dotted', label = 'mean integer')
+if mean2_included:
+    ax.plot(red_time, red_mean2, color='m', linewidth = 1, label = 'mean2')
 ax.set_xlim(left=0)
 ax.set_ylim(bottom=0, top=max_value)
-ax.set(yticks=np.arange(0,max_value,20))
+#ax.set(yticks=np.arange(0,max_value,20))
 ax.grid(True)
 ax.legend()
 plt.title(csv_name)
